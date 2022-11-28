a28e332d917d4568898686c462126ba8b06e1dde
Suvdmaa <e.suvd1230@gmail.com>
2022-11-28 23:52:18 +0800

    gertee hiisen

---
+++ javascript/js-basic/js/datatypes.js
@@ -0,0 +1,43 @@
+let apple = 20;
+console.log(typeof (apple));
+
+
+time = false;
+console.log(typeof(time));
+
+time = "Hi JavaScript";
+console.log(typeof(time));
+
+let number;
+console.log(typeof(number));
+
+let call;
+console.log(call);
+
+var firstName = 'Sarnai' , lastName = 'Bold' , maritalStatus = 'married', conutry = 'Mongolia', age = '35';
+
+var firstName = 'Sar';
+var lastName = 'Bat';
+var maritalStatus = 'Single';
+var country = 'Mongolia';
+var age = '25';
+
+console.log(firstName);
+console.log(lastName);
+console.log(maritalStatus);
+console.log(country);
+console.log(age);
+
+var myAge = 23, yourAge = 20;
+console.log(myAge);
+console.log(yourAge);
+
+let y = 20;
+let x = 10;
+
+console.log(((y * y) - y) / ((x * x) - x))
+
+let bit = 8589934592;
+let gegabyte = 15;
+
+console.log(bit * gegabyte);
---
+++ javascript/js-basic/js/variable.js
@@ -0,0 +1,11 @@
+let message = 'Hello Javascript';
+message = true;
+message = 200;
+console.log(message);
+
+
+let nuuts;
+console.log(nuuts);
+
+
+
