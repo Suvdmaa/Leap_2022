ed7db8d0fe2a27bc5142654ecc5beae8e4a6bf44
Suvdmaa <e.suvd1230@gmail.com>
2022-11-30 15:10:08 +0800

    made changes

--- javascript/js-number/register-number-validation/js/main.js
+++ javascript/js-number/register-number-validation/js/main.js
@@ -6,7 +6,7 @@ const registerNum = registerNumber.substr(2, 9);
 console.log(registerChar);
 console.log(registerNum);
 
-if(!Number.isInteger(Number(registerChar) && Number.isInteger(Number(registerNum))) && registerNumber.length == 10){
+if(!Number.isInteger(Number(registerChar)) && Number.isInteger(Number(registerNum)) && registerNumber.length == 10){
     console.log("Your register number are correct");
 } else {
     console.log("Your register number are wrong");
