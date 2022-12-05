9720e62bc6b4daea050d683745ee0788ba784f1c
Suvdmaa <e.suvd1230@gmail.com>
2022-12-06 01:33:33 +0800

    did in home

--- javascript/js-loop/assignment-3/js/main.js
+++ javascript/js-loop/assignment-3/js/main.js
@@ -1,26 +1,9 @@
-// let n = Number(prompt("Give me a number"));
-// let count = 1;
+let n = Number(prompt("Give me a number"));
 
-
-// while(n <= 100){
-//     if(n / n == 1){
-//         console.log(n);
-//     }
-//     number++;
-// }
-
-// console.log(Number.isInteger(number)); 
-
-
-let n = Number(prompt("Give me  number"));
-
-while(n <= 100){ 
-    console.log(n);
-    n++
-    if(n / n == 1){
-        console.log(n);
-        
-    }
+for(let i =1; i < 100; i++){
+    if (n % i == 0){
+            console.log(i, "is not a prime number");
+        }
+        console.log(i, "is a prime number");
 }
 
-console.log(Number.IsInteger(n));
