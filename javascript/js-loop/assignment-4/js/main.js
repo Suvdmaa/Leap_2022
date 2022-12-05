558c9d1ee1481ee3e56676f82774e3e01ed2116c
Suvdmaa <e.suvd1230@gmail.com>
2022-12-06 00:44:46 +0800

    js-loop assignment-4 d uurchlult hiisen

--- javascript/js-loop/assignment-4/js/main.js
+++ javascript/js-loop/assignment-4/js/main.js
@@ -1,5 +1,10 @@
 let n = [1, 2, 3, 4, 5];
 
+for(let count = 0; count < n.length; count++){
+    console.log(n[count]);
+}
+
+console.log("Start's here");
 
 for(let count = n.length - 1; count >= 0; count--){
     console.log(n[count]);
