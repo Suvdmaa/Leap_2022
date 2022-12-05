5b87ff6b5f264e144862b71216a4e98bade45769
Suvdmaa <e.suvd1230@gmail.com>
2022-12-06 00:33:16 +0800

    made changes

--- javascript/js-array/simple-array-1/js/main.js
+++ javascript/js-array/simple-array-1/js/main.js
@@ -3,21 +3,21 @@ let itCompanies = ["Facebook ", "Google ", "Microsoft ", "Apple ", "IBM ", "Orac
 console.log(itCompanies);
 console.log(itCompanies.length);
 
-console.log("Next...........................");
+console.log("Next...............");
 
 console.log(itCompanies[0]);
 console.log(itCompanies[Math.ceil(itCompanies.length / 2)]);
 console.log(itCompanies[itCompanies.length - 1]);
 
-console.log("Next...........................");
+console.log("Next...............");
 
-for(let count = 0; count < number.length; count++){
+for(let count = 0; count < itCompanies.length; count++){
     if(count < itCompanies.length){
         console.log(itCompanies[count]);
     }
 }
 
-console.log("Next...........................");
+console.log("Next...............");
 
 for(let i = 0; i < itCompanies.length; i++){
     if(i < itCompanies.length){
@@ -25,7 +25,7 @@ for(let i = 0; i < itCompanies.length; i++){
     }
 }
 
-console.log("Next............................");
+console.log("Next...............");
 
 
 console.log(itCompanies + " " + "зэрэг мэдээллийн технологийн томоохон компаниуд");
