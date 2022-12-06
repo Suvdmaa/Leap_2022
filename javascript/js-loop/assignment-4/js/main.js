eb3130a44afba346914fbc0d689ad3bc9b2817bf
Suvdmaa <e.suvd1230@gmail.com>
2022-12-06 19:54:39 +0800

    js-loop dotor assignment-4 hiisen

---
+++ javascript/js-loop/assignment-4/js/main.js
@@ -0,0 +1,15 @@
+// 1. Цифрүүд зайгаар тусгаарлагдана хэвлэгдэнэ.
+
+let result = " ";
+
+for(let input = 0; input < 4; input += 1){
+    result = result + input;
+}
+console.log(result);
+
+
+// 2. Есөөс цифр хүртэл Есийн цифрээс өгөгдсөн цифр хүртэл нэг нэгээр нэг мөрөнд хэвлэ.
+
+for (let i = 5; i >= 5 && i <= 9; i += 1){
+    console.log(i);
+}
