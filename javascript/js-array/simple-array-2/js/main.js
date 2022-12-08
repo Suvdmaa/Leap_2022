3d24ed0670ab53c535a121b61810a8badac5f429
Suvdmaa <e.suvd1230@gmail.com>
2022-12-08 21:41:26 +0800

    did little change in simple-array-2

--- javascript/js-array/simple-array-2/js/main.js
+++ javascript/js-array/simple-array-2/js/main.js
@@ -1,6 +1,7 @@
 // Array - ын хувьсагчийг itCompanies гэж зарлаж, Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon зэрэг анхны утгыг оруулна уу.
 
 let itCompanies = ["Facebook ", "Google ", "Microsoft ", "Apple ", "IBM ", "Oracle ", "Amazon "];
+console.log(itCompanies)
 
 // Array - аас эхний 3 компанийг хайчилж ав
 console.log(itCompanies[0], itCompanies[1], itCompanies[2],);
@@ -9,13 +10,15 @@ console.log(itCompanies[0], itCompanies[1], itCompanies[2],);
 console.log(itCompanies[itCompanies.length - 1], itCompanies[itCompanies.length - 2] ,itCompanies[itCompanies.length - 3],);
 
 // Мэдээллийн технологийн дунд компани эсвэл компаниудыг arrey - аас хайчилж ав
-for (let i = 0; i < itCompanies.length; i++){
-    console.log(itCompanies[i])
-}
+arr = itCompanies.length / 2
+itCompanies.splice(arr, 1);
+console.log(itCompanies)
 
 // Эхний мэдээллийн технологийн компанийг array - аас хас
 itCompanies.shift();
 console.log(itCompanies);
+
+
 
 // Сүүлийн мэдээллийн технологийн компанийг array - аас хас
 itCompanies.pop();
