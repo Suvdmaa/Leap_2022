49d86bd6e8719400b1045bc9dce084b12b1e4418
Suvdmaa <e.suvd1230@gmail.com>
2022-12-12 16:09:10 +0800

    color-generator hiisen

---
+++ javascript/js-dom/color-generator/js/main.js
@@ -0,0 +1,6 @@
+function randomColorGenerator() {
+    const randomColor = Math.floor(Math.random()*16777215).toString(16);
+    document.body.style.backgroundColor = "#" + randomColor;
+  }
+  
+btn.addEventListener("click", randomColorGenerator);
