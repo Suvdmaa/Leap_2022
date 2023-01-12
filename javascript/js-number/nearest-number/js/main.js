b9354ff10c46813f2b8c56874ef9b6d51c0032f0
Suvdmaa <e.suvd1230@gmail.com>
2023-01-12 22:28:19 +0800

    nearest number hiisen

---
+++ javascript/js-number/nearest-number/js/main.js
@@ -0,0 +1,10 @@
+// n бутархай тооны хамгийн ойрын бүхэл тоог ол.
+
+let n = Math.round(5.6)
+console.log(n)
+
+let x = Math.round(2.3)
+console.log(x)
+
+let m = prompt("Бутархай тоо өгнө үү")
+console.log(Math.round(m))
