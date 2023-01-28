a2f4d87eb1eedca38d35dc23f7ef65ac327c6096
Suvdmaa <e.suvd1230@gmail.com>
2023-01-28 20:17:23 +0800

    add

--- javascript/js-loop/square/js/main.js
+++ javascript/js-loop/square/js/main.js
@@ -1,12 +1,32 @@
 let n = Number(prompt("Enter number of rows"));
 
-let result = " ";
 
-for(let a = 0; a < n; a = a + a){
-    a = "*";
-    result = result + a;
+
+for(let i = 0; i < n; i++){
+    let row = " ";
+    for(let j = 0; j < n; j++){
+        row += " *"
+    }
+   console.log(row)
 }
-console.log(result);
 
+
+for(let i = 0; i < n; i++){
+    let column = " *"
+    for(let j = 0; j < n; j++){
+        column += " ";
+    }
+    column += " *"
+    console.log(column)
+}
+
+
+for(let i = 0; i < n; i++){
+    let row = " ";
+    for(let j = 0; j < n; j++){
+        row += " *"
+    }
+   console.log(row)
+}
 
 
